57240b0d1c77ff62d5af3b8008e5d081fec29459
Yuri <hyuridesousa@gmail.com>
2022-07-15 16:02:37 -0300

    Landing page

---
+++ script.js
@@ -0,0 +1,12 @@
+function imprimir(){
+    var nome = document.getElementById("nome");
+    var sobrenome = document.getElementById("sobrenome");
+    var email = document.getElementById("email");
+    var endereco = document.getElementById("endereco");
+    var senha = document.getElementById("senha");
+
+    
+    
+    alert("seu nome é " + nome.value + "\n" + "Seu Sobrenome é" + sobrenome.value + "\n" + "Seu email é " + email.value + "\n" + "Seu endereco é " + "\n" + endereco.value + "Sua senha é " + senha.value);
+    
+}
